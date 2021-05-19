e17a8798e0064e2d4a3cfd3adfcb3c9e13b2979b
ss15859 <ss15859@bristol.ac.uk>
2021-05-19 11:02:07 +0100

    add comments to sirc

--- src/sirc.cpp
+++ src/sirc.cpp
@@ -3,12 +3,16 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericMatrix sirc(NumericMatrix theta, double N, double tf, bool initial = true){
-  
+
+// initialising variables
+      
   double S0 = N - 1;
   double I0 = 1;
   double R0 = 0;
   
   int Nruns = theta.nrow();
+  
+  // return error if theta is of incorrect dimension for initial run
   
   if(Nruns>1 && initial == true){
     
@@ -17,12 +21,16 @@ NumericMatrix sirc(NumericMatrix theta, double N, double tf, bool initial = true
     return -1;
   }
   
+  // initialising output elements
+  
   NumericVector IR;
   
   NumericMatrix X(Nruns, 2*tf);
   
   int n;
 
+  // loop over the number of runs
+  
   for (int i = 0; i < Nruns; i++){
   
     double beta = theta(i,0);
@@ -40,10 +48,15 @@ NumericMatrix sirc(NumericMatrix theta, double N, double tf, bool initial = true
 
     
     do{
+      
+      // append the new populations to the vectors
+      
       ta.push_back(t);
       Sa.push_back(S);
       Ia.push_back(I);
       Ra.push_back(R);
+      
+      // calculate the updates
       
       double lambd = beta*(I+iota)/N;
       double ifrac = 1.0 -exp(-lambd);
@@ -51,16 +64,22 @@ NumericMatrix sirc(NumericMatrix theta, double N, double tf, bool initial = true
       double  infection = as<double>(rbinom(1,S,ifrac));
       double recovery = as<double>(rbinom(1,I,rfrac));
       
+      // updates the populations
+      
       S-=infection;
       I= I +infection - recovery;
       R+= recovery;
       t++;
+      
+      // conditional break on initial run
       
       if(initial == true){
         if(I==0){break;}
       }
       
     } while (t<=tf );
+    
+    // concatenating the I and R vector. This is our sumarary statistic.
     
     NumericVector IR(Ia.size() + Ra.size()); // preallocate memory
   
@@ -73,6 +92,8 @@ NumericMatrix sirc(NumericMatrix theta, double N, double tf, bool initial = true
   
       IR.insert(j+Ia.size(),Ra[j]);
     }
+    
+    // adding the summary statistic to the output matrix
     
     X(i,_) =  IR[Range(0,2*Ia.size())];
     
